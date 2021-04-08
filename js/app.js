77a59688a812bff2d2338725c7f046246c0f0586
kseiler <kseiler@kseiler.local>
2021-04-07 22:41:31 -0700

    clean up of scss files and backdrop filter fix for css validation error

--- js/app.js
+++ js/app.js
@@ -1,36 +1,27 @@
 baguetteBox.run('.gallery');
 
-
+// Set keyup listener to Search box
 const search = document.getElementById('search');
 search.addEventListener('keyup', filterImages);
 
-// function filterImages() {
-//   let filter = document.getElementsByTagName('search').value
-//   filter = filter.toLowerCase();
-//   let item = document.getElementsByClassName('gallery-item');
-//   let caption = document.getElementsByTagName('a')[0].getAttribute('data-caption')[0];
-
-//   for ( i = 0; i < caption.length; i++ ) {
-//     if ( caption[i].toLowerCase().includes(filter) ) {
-//       item[i].style.display = '';
-//     } else {
-//       item[i].style.display = 'none';
-//     }
-//   }
-  
-// }
+/**
+ * Filter Image Function
+ * Takes search input and filters images based on keywords from image captions.
+ */
 
 function filterImages() {
-  let input = document.getElementById('search').value;
-  input = input.toLowerCase();
-  let link = document.getElementsByTagName('a');
-    
-  for (i = 0; i < link.length; i++) { 
-      if (!link[i].getAttribute('data-caption').toLowerCase().includes(input)) {
-          link[i].style.display="none";
-      }
-      else {
-          link[i].style.display="";                 
-      }
-  }
+    // Take 'search string' and format to lowercase
+    let input = document.getElementById('search').value;
+    input = input.toLowerCase();
+    // Target links
+    let link = document.getElementsByTagName('a');
+
+    for ( let i = 0; i < link.length; i++ ) { 
+        if ( link[i].getAttribute('data-caption').toLowerCase().includes(input) ) {
+            link[i].style.display = "";
+        }
+        else {
+            link[i].style.display = "none";                 
+        }
+    }
 }
