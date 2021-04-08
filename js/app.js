415929492af4a760bfabcb33108ccfe03a95aa1d
kseiler <kseiler@kseiler.local>
2021-04-07 22:54:24 -0700

    final comments on JS file

--- js/app.js
+++ js/app.js
@@ -15,11 +15,14 @@ function filterImages() {
     input = input.toLowerCase();
     // Target links
     let link = document.getElementsByTagName('a');
-
+    // Loop through each link and target the caption attribute
+    // Convert caption to lowercase and compare with input string
     for ( let i = 0; i < link.length; i++ ) { 
+        // if string matches do nothing (show content)
         if ( link[i].getAttribute('data-caption').toLowerCase().includes(input) ) {
             link[i].style.display = "";
         }
+        // if no match then hide
         else {
             link[i].style.display = "none";                 
         }
