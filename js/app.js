4ab1deadd8dc86f6352b7903155dd70b8a3eab3d
kseiler <kseiler@kseiler.local>
2021-04-07 22:58:03 -0700

    updated comments

--- js/app.js
+++ js/app.js
@@ -13,16 +13,16 @@ function filterImages() {
     // Take 'search string' and format to lowercase
     let filter = document.getElementById('search').value;
     filter = filter.toLowerCase();
-    // Target links
+    // Target <a> HTML elements
     let link = document.getElementsByTagName('a');
-    // Loop through each link and target the caption attribute
+    // Loop through each <a> and get strings from within "data-caption" attribute
     // Convert caption to lowercase and compare with filter string
     for ( let i = 0; i < link.length; i++ ) { 
         // if string matches do nothing (show content)
         if ( link[i].getAttribute('data-caption').toLowerCase().includes(filter) ) {
             link[i].style.display = "";
         }
-        // if no match then hide
+        // if no match then display: none;
         else {
             link[i].style.display = "none";                 
         }
